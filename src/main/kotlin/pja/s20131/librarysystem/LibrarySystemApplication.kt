b63cd2add79383bda0a685996e52f0fb14d449eb
Jakub Augustyniak <kubaugust@gmail.com>
2022-12-16 20:28:10 +0100

    Setup project

---
+++ src/main/kotlin/pja/s20131/librarysystem/LibrarySystemApplication.kt
@@ -0,0 +1,11 @@
+package pja.s20131.librarysystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibrarySystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibrarySystemApplication>(*args)
+}
