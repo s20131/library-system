289dec0808273711455daf0ff12ec9021225fbfc
Jakub Augustyniak <kubaugust@gmail.com>
2023-02-04 01:44:27 +0100

    Implement RestControllerAdvice and ExceptionMapper

--- src/main/kotlin/pja/s20131/librarysystem/domain/user/UserService.kt
+++ src/main/kotlin/pja/s20131/librarysystem/domain/user/UserService.kt
@@ -1,19 +1,24 @@
 package pja.s20131.librarysystem.domain.user
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import pja.s20131.librarysystem.adapter.database.user.UserRepository
+import pja.s20131.librarysystem.infrastructure.exception.NotFoundException
 
 @Service
+@Transactional
 class UserService(
     val userRepository: UserRepository
 ) {
 
     fun getUser(userId: UserId): UserBasicData {
-        return userRepository.get(userId) ?: throw RuntimeException("User not found")
+        return userRepository.get(userId) ?: throw UserNotFoundException(userId)
     }
 
     fun getUserSettings(userId: UserId): UserSettings {
-        return userRepository.getSettings(userId) ?: throw RuntimeException("User not found")
+        return userRepository.getSettings(userId) ?: throw UserNotFoundException(userId)
     }
 
 }
+
+class UserNotFoundException(id: UserId) : NotFoundException("User with id=${id.value} not found")
