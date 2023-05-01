f0e241eceab2535648aae5a9eda2f73f028b4ea3
Jakub Augustyniak <kubaugust@gmail.com>
2023-05-01 23:28:51 +0200

    Replace h2 test database with postgres and testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ repositories {
 val exposed = "0.41.1"
 val postgres = "42.6.0"
 val jackson = "2.15.0"
-val h2 = "2.1.214"
 val faker = "1.0.2"
 val archunit = "1.0.1"
+val testcontainers = "1.18.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -35,9 +35,10 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgres")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.h2database:h2:$h2")
     testImplementation("com.github.javafaker:javafaker:$faker")
     testImplementation("com.tngtech.archunit:archunit-junit5:$archunit")
+    testImplementation("org.testcontainers:junit-jupiter:$testcontainers")
+    testImplementation("org.testcontainers:postgresql:$testcontainers")
 }
 
 tasks.withType<KotlinCompile> {
