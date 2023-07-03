e8d988037e2a4a3c7c1418b08bac53e0d62053d7
Jakub Augustyniak <kubaugust@gmail.com>
2023-07-03 22:00:22 +0200

    Add cron job expression to the configuration file

--- src/test/kotlin/pja/s20131/librarysystem/rental/PenaltyServiceTests.kt
+++ src/test/kotlin/pja/s20131/librarysystem/rental/PenaltyServiceTests.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
+import org.testcontainers.shaded.org.awaitility.Awaitility
 import pja.s20131.librarysystem.Assertions
 import pja.s20131.librarysystem.BaseTestConfig
 import pja.s20131.librarysystem.Preconditions
@@ -11,6 +12,7 @@ import pja.s20131.librarysystem.domain.resource.PenaltyService
 import pja.s20131.librarysystem.domain.resource.model.Penalty
 import pja.s20131.librarysystem.domain.resource.model.RentalPeriod
 import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
 
 @SpringBootTest
 @Sql("/sql/update_penalties.sql")
@@ -25,8 +27,11 @@ class PenaltyServiceTests @Autowired constructor(
         val user = given.user.exists().build()
         val book = given.author.exists().withBook().build().second[0]
         val library = given.library.exists().hasCopy(book.resourceId).build()
-        val rental = given.rental.exists(user.userId, book.resourceId, library.libraryId, RentalPeriod.startRental(clock.monthAgo()), penalty = Penalty(
-            BigDecimal(2.50)))
+        val rental = given.rental.exists(
+            user.userId, book.resourceId, library.libraryId, RentalPeriod.startRental(clock.monthAgo()), penalty = Penalty(
+                BigDecimal(2.50)
+            )
+        )
 
         penaltyService.updatePenaltiesForResourceOverdue()
 
@@ -34,7 +39,21 @@ class PenaltyServiceTests @Autowired constructor(
     }
 
     @Test
-    fun `should calculate and set penalty if was null`() {
+    fun `should run update penalties procedure using cron`() {
+        val user = given.user.exists().build()
+        val book = given.author.exists().withBook().build().second[0]
+        val library = given.library.exists().hasCopy(book.resourceId).build()
+        val rental = given.rental.exists(user.userId, book.resourceId, library.libraryId, RentalPeriod.startRental(clock.monthAgo()))
+
+        Awaitility.await()
+            .atMost(3, TimeUnit.SECONDS)
+            .untilAsserted {
+                assert.rental.isSaved(user.userId, book.resourceId, library.libraryId, rental.rentalPeriod, rental.rentalStatus, Penalty(BigDecimal("2.50")))
+            }
+    }
+
+    @Test
+    fun `should calculate and set penalty if it was null`() {
         val user = given.user.exists().build()
         val book = given.author.exists().withBook().build().second[0]
         val library = given.library.exists().hasCopy(book.resourceId).build()
