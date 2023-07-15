508157f0d713e27fe4888b8fd6873b6c547b2d7d
Jakub Augustyniak <kubaugust@gmail.com>
2023-07-15 19:56:04 +0200

    Fix fake clock implementation in the database

--- src/test/resources/sql/revoke_awaiting_resources.sql
+++ src/test/resources/sql/revoke_awaiting_resources.sql
@@ -1,3 +1,5 @@
+SET TIMEZONE TO 'Europe/Warsaw';
+
 CREATE OR REPLACE PROCEDURE revoke_awaiting_resources()
 LANGUAGE plpgsql
 AS '
@@ -5,6 +7,6 @@ AS '
     BEGIN
         UPDATE rental
         SET status = ''CANCELLED''
-        WHERE status = ''RESERVED_TO_BORROW'' AND finish < CURRENT_TIMESTAMP;
+        WHERE status = ''RESERVED_TO_BORROW'' AND finish < ''2023-05-07 17:55:00.000''::timestamptz;
     END;
 ';
