2fee5df243885143cb029206e90da0913aa5c06c
Jakub Augustyniak <kubaugust@gmail.com>
2023-09-04 12:39:21 +0200

    Add index example

--- src/main/resources/sql/04_db_objects_post.sql
+++ src/main/resources/sql/04_db_objects_post.sql
@@ -5,3 +5,5 @@ CREATE INDEX author_covering_idx ON author(first_name, last_name) INCLUDE (id);
 -- CREATE INDEX book_isbn_hash_idx ON book USING HASH(isbn); -- only example as this is already unique column
 
 CREATE INDEX settings_idx ON internal.config USING GIST(settings);
+
+-- CREATE INDEX library_card_idx ON library_card USING BRIN(number); -- only example as this is already unique column
